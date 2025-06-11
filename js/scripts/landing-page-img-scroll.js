2f2fe1286c693627f198f94a3001b22045cbc07e
kadersahib <kadermaraikar99@gmail.com>
2025-06-11 14:36:43 +0530

    change file in css and js file

--- js/scripts/landing-page-img-scroll.js
+++ js/scripts/landing-page-img-scroll.js
@@ -1,17 +1,4 @@
-  // const slider = document.querySelector('.slider');
-  // let scrollAmount = 0;
-  // const slideWidth = slider.offsetWidth;
-
-  // setInterval(() => {
-  //   scrollAmount += slideWidth;
-  //   if (scrollAmount >= slider.scrollWidth) {
-  //     scrollAmount = 0;
-  //   }
-  //   slider.scrollTo({
-  //     left: scrollAmount,
-  //     behavior: 'auto'
-  //   });
-  // }, 3000); // Change image every 3 seconds
+ 
 const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
