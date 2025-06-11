da437f268dd47eaa59ded91736b96fe98eca73a3
kadersahib <kadermaraikar99@gmail.com>
2025-06-11 13:57:01 +0530

    change file in css

--- js/scripts/landing-page-img-scroll.js
+++ js/scripts/landing-page-img-scroll.js
@@ -1,14 +1,22 @@
-  const slider = document.querySelector('.slider');
-  let scrollAmount = 0;
-  const slideWidth = slider.offsetWidth;
+  // const slider = document.querySelector('.slider');
+  // let scrollAmount = 0;
+  // const slideWidth = slider.offsetWidth;
 
-  setInterval(() => {
-    scrollAmount += slideWidth;
-    if (scrollAmount >= slider.scrollWidth) {
-      scrollAmount = 0;
-    }
-    slider.scrollTo({
-      left: scrollAmount,
-      behavior: 'auto'
-    });
-  }, 3000); // Change image every 3 seconds
+  // setInterval(() => {
+  //   scrollAmount += slideWidth;
+  //   if (scrollAmount >= slider.scrollWidth) {
+  //     scrollAmount = 0;
+  //   }
+  //   slider.scrollTo({
+  //     left: scrollAmount,
+  //     behavior: 'auto'
+  //   });
+  // }, 3000); // Change image every 3 seconds
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+setInterval(() => {
+  slides[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('active');
+}, 3000);
